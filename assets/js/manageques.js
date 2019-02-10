411f7e06ae660476989d0c13141217aaed7dc490
neelgeek <neelbhave91@gmail.com>
2019-02-10 12:07:21 +0530

    Limiting added to Loadtable

--- assets/js/manageques.js
+++ assets/js/manageques.js
@@ -1,3 +1,10 @@
+const table = $('#data_table').dataTable();
+const dtable = $('#data_table').DataTable();
+var current_count = 11;
+$(document).ready(function() {
+    loadTable(11);
+});
+
 function deleteQues(id) {
     console.log(id);
     $.ajax({
@@ -12,19 +19,14 @@ function deleteQues(id) {
     });
 };
 
-function editQues(id) {
-    window.location = "addquestion.html?id=" + id;
-}
 
-$(document).ready(function() {
-    var table = $('#data_table').dataTable();
+
+function loadTable(count = 11) {
     $.ajax({
         type: "GET",
-        url: "https://app-quizzy.herokuapp.com/view/questions",
+        url: "https://app-quizzy.herokuapp.com/view/questions?count=" + count,
         data: {},
         success: function(response) {
-
-
             table.fnClearTable();
             response.forEach(element => {
                 table.fnAddData([
@@ -36,8 +38,27 @@ $(document).ready(function() {
                     "<div class=\"row\"><div class=\"col-md-4\"><div><a onclick=\"return editQues(\'" + element._id + "')\" class=\"btn btn-success btn-icon btn-circle btn-lg\"><i class=\" fa fa fa-cog \"></i></a></div></div><div class=\"col-md-4\"><div><a data-click=\"swal-danger\" onclick=\"return deleteQues(\'" + element._id + "')\" class=\"btn btn-danger btn-icon btn-circle btn-lg\"><i class=\" fa fa fa-trash-o \"></i></a></div></div></div>"
                 ]);
             });
-
         }
     });
+
+}
+
+
+function editQues(id) {
+    window.location = "addquestion.html?id=" + id;
+}
+
+
+
+
+$("#data_table").on('page.dt', function(e) {
+    e.preventDefault();
+    var info = dtable.page.info();
+    var count = (info.page * 10) + 11;
+    if (count > current_count) {
+        current_count = count;
+        loadTable(count);
+    }
+    console.log(count);
 
 });
